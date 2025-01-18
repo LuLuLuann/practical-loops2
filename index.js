eb13b4e32202c81b7cbad6b6ddb130afcf94224e
LuLuLuann <lu.akaydin@gmail.com>
2025-01-17 22:07:22 -0500

    fixed file

--- index.js
+++ index.js
@@ -26,14 +26,12 @@ console.log("----------PART 2: PRIME TIME ---------")
 
 // Write a script that accomplishes the following:
 // Declare an arbitrary number, n.
-<<<<<<< HEAD
 // Create a loop that searches for the next prime number, starting at n and incrementing from there.
 // As soon as you find the prime number, log that number and exit the loop.
 // Continuing with the example above, if n is equal to 4, your loop should log 5. Similarly, if n is 5, it should log 7, and if n is 9, it should log 11. Test your loop with higher numbers and reference an online prime number table to determine the accuracy of your code.
 // Be careful! If you set n to a number too large, your loop could take a long time to process.
 
 // PART 3: Feeling Loopy
-=======
 
 // Create a loop that searches for the next prime number, starting at n and incrementing from there.
 
@@ -179,4 +177,3 @@ console.log(cell1);
 console.log(cell2);
 console.log(cell3);
 console.log(cell4);
->>>>>>> 2761c7b (part3)
